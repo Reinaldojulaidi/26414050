30aca38b3ca42ed702dca4d851a200d004bb77dd
Reinaldojulaidi <Reinaldojulaidi@users.noreply.github.com>
2016-09-21 23:07:12 +0700

    coba sorting with perl

---
+++ cobasort.pl
@@ -0,0 +1,7 @@
+@word = <STDIN>;
+print sort (@word);[D[D[D[D[D[D[D[D[D[[A[D[D[D[D[D[D[D[D[D[D[D[D[D
+[A
+
+[A[A#!/usr/bin/perl[3~[[B[A
+@word = <STDIN>;[3~[3~[3~[C[D
+printt sort(@word);
